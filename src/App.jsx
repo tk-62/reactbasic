cd3bed67c728b48ad474fabf884bf50f41c73622
tk <68478701+tk-62@users.noreply.github.com>
2021-10-28 21:16:27 +0900

    css追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "72px",
+    backgroundColor: "#c0ddee"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか?</p>
-      <button>ボタン</button>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
